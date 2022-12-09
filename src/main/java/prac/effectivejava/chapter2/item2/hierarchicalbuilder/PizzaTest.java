7e0e1761476d7f6401826a6d23299740666e4b36
Kimjunghwoi <poiuy5947@gmail.com>
2022-12-09 18:08:34 +0900

    feat: Item 2. 생성자에 매개변수가 많다면 빌더를 고려하라
    점층적 생성자 패턴과 자바 빈즈의 장점을 합친 빌더에 대해 정리했습니다.

---
+++ src/main/java/prac/effectivejava/chapter2/item2/hierarchicalbuilder/PizzaTest.java
@@ -0,0 +1,17 @@
+package prac.effectivejava.chapter2.item2.hierarchicalbuilder;
+
+import static prac.effectivejava.chapter2.item2.hierarchicalbuilder.NyPizza.Size.SMALL;
+import static prac.effectivejava.chapter2.item2.hierarchicalbuilder.Pizza.Topping.*;
+
+// 계층적 빌더 사용 (21쪽)
+public class PizzaTest {
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(SMALL)
+                .addTopping(SAUSAGE).addTopping(ONION).build();
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(HAM).sauceInside().build();
+        
+        System.out.println(pizza);
+        System.out.println(calzone);
+    }
+}
